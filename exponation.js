a8d463ef9348e69fea159303d581a2a15e0d1bfd
Kirill Osin <59966999+yourProgrammist@users.noreply.github.com>
2023-12-15 00:23:48 +0300

    Delete exponation.js

--- exponation.js
+++
@@ -1,9 +0,0 @@
-const pow = (x, n) => {
-    if (n === 0)
-        return 1;
-    if (n % 2 === 0) {
-        const res = pow(x, n / 2);
-        return res * res;
-    } 
-    return x * pow(x, n - 1);
-}
